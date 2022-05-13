239893a8c68f3fb0f3360c1fa0407f72c4c285f5
Si Thu Zaw <sithuzaw200258@gmail.com>
2022-05-13 20:12:11 +0630

    first commit

---
+++ actions.js
@@ -0,0 +1,18 @@
+let body = document.querySelector("body"),
+    lightBox = document.querySelector(".lightBox"),
+    img = document.querySelectorAll(".gImg"),
+    showImg = lightBox.querySelector(".showImg img"),
+    close = lightBox.querySelector(".close");
+
+for (let image of img) {
+    image.addEventListener("click", () => {
+        lightBox.style.display = "block";
+        showImg.src = image.src;
+        body.style.overflow = "hidden";
+
+        close.addEventListener("click", () => {
+            lightBox.style.display = "none";
+            body.style.overflow = "visible";
+        })
+    })
+}
